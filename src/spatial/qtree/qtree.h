cb8ee3120a1544246f717bd238153b65dce1532f
DavidMeagher1 <meagherjdavid@gmail.com>
2026-04-06 21:15:14 -0700

    refactor(qtree): reorganize namespace structure for clarity and consistency

--- src/spatial/qtree/qtree.h
+++ src/spatial/qtree/qtree.h
@@ -6,46 +6,44 @@
 #include <godot_cpp/variant/vector2i.hpp>
 #include <godot_cpp/variant/rect2i.hpp>
 
-namespace l_systems{
-    namespace spatial {
-        struct QCell {
-            int x;
-            int y;
-            q16norm_t u;
-            q16norm_t v;
-        };
+namespace l_systems::spatial {
+    struct QCell {
+        int x;
+        int y;
+        q16norm_t u;
+        q16norm_t v;
+    };
 
-        enum Quadrant {
-            NW = 0,
-            NE = 1,
-            SW = 2,
-            SE = 3
-        };
+    enum Quadrant {
+        NW = 0,
+        NE = 1,
+        SW = 2,
+        SE = 3
+    };
 
-        class QNode {
-            public:
-                QNode();
-                ~QNode();
+    class QNode {
+        public:
+            QNode();
+            ~QNode();
 
-                unsigned int parent = 0;
-                
-                unsigned int children[4] = {0, 0, 0, 0};
-                QCell data;
-            private:
-        };
+            unsigned int parent = 0;
+            
+            unsigned int children[4] = {0, 0, 0, 0};
+            QCell data;
+        private:
+    };
 
-        class QTree {
-            public:
-                QTree();
-                ~QTree();
-                std::vector<QNode> nodes;
+    class QTree {
+        public:
+            QTree();
+            ~QTree();
+            std::vector<QNode> nodes;
 
-                void insert(const QCell &cell);
-                void subdivide(unsigned int node_index);
-                godot::Rect2i get_bounds();
-                void clear();
+            void insert(const QCell &cell);
+            void subdivide(unsigned int node_index);
+            godot::Rect2i get_bounds();
+            void clear();
 
-            private:
-        };
-    }
+        private:
+    };
 }
